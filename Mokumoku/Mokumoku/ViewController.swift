e3b31de7fb730a591d41d9d9ccc83f68f240d837
野崎勇樹 <n0o9z2a1k1ihappylife@gmail.com>
2017-10-21 17:16:04 +0900

    イニシャルコミット

---
+++ Mokumoku/Mokumoku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mokumoku
+//
+//  Created by 野崎勇樹 on 2017/10/21.
+//  Copyright © 2017年 野崎勇樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
