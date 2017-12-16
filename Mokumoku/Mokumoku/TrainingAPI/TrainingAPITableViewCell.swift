39a158f54eae0c8dbefd63a6efd311b0c3efc614
野崎勇樹 <n0o9z2a1k1ihappylife@gmail.com>
2017-12-16 19:58:55 +0900

    カスタムテーブルビューの作成 【用途】 ・API利用

---
+++ Mokumoku/Mokumoku/TrainingAPI/TrainingAPITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TrainingAPITableViewCell.swift
+//  Mokumoku
+//
+//  Created by 野崎勇樹 on 2017/12/14.
+//  Copyright © 2017年 野崎勇樹. All rights reserved.
+//
+
+import UIKit
+
+class TrainingAPITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
