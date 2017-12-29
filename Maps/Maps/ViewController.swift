03b5f1cbb4af15940e43674ec3c141c2a80d8b76
野崎勇樹 <n0o9z2a1k1ihappylife@gmail.com>
2017-12-29 10:43:45 +0900

    マップ作成のためのinit

---
+++ Maps/Maps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Maps
+//
+//  Created by 野崎勇樹 on 2017/12/29.
+//  Copyright © 2017年 野崎勇樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
