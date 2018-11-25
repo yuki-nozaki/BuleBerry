14cdec59f0aa2e0911ad429ae60b7a04a6c65911
野崎勇樹 <n0o9z2a1k1ihappylife@gmail.com>
2018-11-25 20:09:02 +0900

    イベントリスト作成のための初期コミット

---
+++ シンプルなイベントリスト/シンプルなイベントリスト/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  シンプルなイベントリスト
+//
+//  Created by 野崎勇樹 on 2018/11/25.
+//  Copyright © 2018年 野崎勇樹. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ シンプルなイベントリスト/シンプルなイベントリスト/EventRequester.swift
@@ -0,0 +1,13 @@
+//
+//  EventRequester.swift
+//  シンプルなイベントリスト
+//
+//  Created by 野崎勇樹 on 2018/11/25.
+//  Copyright © 2018年 野崎勇樹. All rights reserved.
+//
+
+import Foundation
+
+final class EventRequester: NSObject {
+    
+}
