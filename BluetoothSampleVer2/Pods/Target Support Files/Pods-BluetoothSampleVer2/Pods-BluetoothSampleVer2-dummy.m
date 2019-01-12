f2b7e8fea839d4fca1d746152663a3591f55cce5
野崎勇樹 <n0o9z2a1k1ihappylife@gmail.com>
2019-01-12 20:32:41 +0900

    BlueToothアプリを

--- BluetoothSampleVer2/Pods/Target Support Files/Pods-BluetoothSampleVer2/Pods-BluetoothSampleVer2-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BluetoothSampleVer2 : NSObject
-@end
-@implementation PodsDummy_Pods_BluetoothSampleVer2
-@end
--- BluetoothSampleVer2Peripheral/Pods/Target Support Files/Pods-BluetoothSampleVer2Peripheral/Pods-BluetoothSampleVer2Peripheral-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BluetoothSampleVer2Peripheral : NSObject
-@end
-@implementation PodsDummy_Pods_BluetoothSampleVer2Peripheral
-@end
