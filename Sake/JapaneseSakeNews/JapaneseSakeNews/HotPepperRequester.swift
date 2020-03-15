1db5423596249400c4ff6acdc70a78ef4f6fcaab
YUKI NOZAKI <n0o9z2a1k1ihappylife@gmail.com>
2020-03-15 16:28:48 +0900

    不要なアプリを削除

--- Sake/JapaneseSakeNews/JapaneseSakeNews/HotPepperRequester.swift
+++
@@ -1,9 +0,0 @@
-//
-//  HotPepperRequester.swift
-//  JapaneseSakeNews
-//
-//  Created by 野崎勇樹 on 2019/03/10.
-//  Copyright © 2019年 野崎勇樹. All rights reserved.
-//
-
-import Foundation
--- Sake/JapaneseSakeNews/UnitTests/HotPepperRequesterTests.swift
+++
@@ -1,9 +0,0 @@
-//
-//  HotPepperRequesterTests.swift
-//  JapaneseSakeNewsTests
-//
-//  Created by 野崎勇樹 on 2019/03/16.
-//  Copyright © 2019年 野崎勇樹. All rights reserved.
-//
-
-import Foundation
